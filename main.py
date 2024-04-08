d41dad5f899fa4e886cc9e0633746e451f321b87
Bitboy7 <98996489+Bitboy7@users.noreply.github.com>
2024-04-07 20:06:15 -0600

    main

--- main.py
+++ main.py
@@ -170,7 +170,7 @@ flow = Flow.from_client_secrets_file(
             "https://www.googleapis.com/auth/userinfo.email",
             "openid"
             ],
-    redirect_uri="http://127.0.0.1:5000/callback"
+    redirect_uri="mongodbfllask-production.up.railway.app/callback"
 )
 
 
