0aca09927650229e3fe12d304483be87db2f024b
Bitboy7 <98996489+Bitboy7@users.noreply.github.com>
2024-04-07 21:26:17 -0600

    uploads

--- main.py
+++ main.py
@@ -18,7 +18,7 @@ load_dotenv()
 # Declaramos la variable de ejecución de la aplicación
 app = Flask(__name__)
 
-UPLOAD_FOLDER = 'static'
+UPLOAD_FOLDER = '/uploads'
 
 os.environ["OAUTHLIB_INSECURE_TRANSPORT"] = "1"
 
@@ -36,7 +36,7 @@ def guardar_archivo(file):
     filename = secure_filename(file.filename.replace(" ", "_"))
     
     # Guardar el archivo en la carpeta uploads dentro de static
-    file.save(os.path.join(app.config.get('UPLOAD_FOLDER', 'static'), filename))
+    file.save(os.path.join(app.config.get('UPLOAD_FOLDER', '/uploads'), filename))
     
     # Guardar el archivo en el sistema de archivos de la base de datos
     fs.put(file, filename=filename, fecha_creacion=fecha_actual)
