f2957db82b92932883b731833729e8c8822c94a0
Bitboy7 <98996489+Bitboy7@users.noreply.github.com>
2024-04-10 00:58:03 -0600

    changes

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
-from flask import Flask, render_template, request, make_response, redirect, abort, session, redirect, url_for
+from flask import Flask, render_template, request, make_response, redirect, abort, session, redirect, url_for, abort
 import oauthlib
-import os
-from gridfs import GridFS
 import os
 from werkzeug.utils import secure_filename
 from datetime import datetime
@@ -12,6 +10,7 @@ from pip._vendor import cachecontrol
 import requests
 import pathlib
 from dotenv import load_dotenv
+from helpers.funciones import *
 import google.auth.transport.requests
 # Cargar las variables de entorno desde el archivo .env
 load_dotenv()
@@ -27,138 +26,6 @@ app.secret_key = os.getenv('SECRET_KEY')
 app.config['UPLOAD_FOLDER'] = UPLOAD_FOLDER
 
 fs = GridFS(db) # Crear un sistema de archivos en la base de datos
-
-
-def guardar_archivo(file):
-    # Obtener la fecha y hora actual
-    fecha_actual = datetime.today()
-    
-    # Reemplazar espacios en el nombre de archivo por _
-    filename = secure_filename(file.filename.replace(" ", "_"))
-    
-    # Guardar el archivo en la carpeta uploads dentro de static
-    file.save(os.path.join(app.config.get('UPLOAD_FOLDER', 'static/uploads'), filename))
-    
-    # Guardar el archivo en el sistema de archivos de la base de datos
-    fs.put(file, filename=filename, fecha_creacion=fecha_actual)
-
-
-def obtener_archivos(extension):
-    # Utiliza la función find de la variable fs para buscar archivos con la extensión especificada
-    files = fs.find({'filename': {'$regex': f'\.{extension}$'}})
-    return files
-
-
-def guardar_post(filename, title, name, description):
-    #  # Obtener la fecha y hora actual
-    fecha_actual = datetime.today().strftime("%d-%m-%Y %H:%M:%S")
-
-    # Obtener el usuario que publica el post
-    author = db.users.find_one({"name": name})
-
-    if author:
-        # Guardar la información del archivo en la colección "posts" relacionándolo al usuario
-        post_data = {
-            "filename": filename,
-            "title": title,
-            "author": author["_id"],
-            "fecha_creacion": fecha_actual,
-            "description": description,
-        }
-        db.posts.insert_one(post_data)
-        return True
-    else:
-        print("No se encontró el usuario que publica el post.")
-        return False
-
-
-def guardar_comentario(filename, comment, name):
-    # Obtener la fecha y hora actual
-    fecha_actual = datetime.today().strftime("%d-%m-%Y %H:%M:%S")
-    
-    # Obtener el usuario que publica el comentario
-    author = db.users.find_one({"name": name})
-    
-    if author:
-        # Obtener el post_id basado en el filename
-        post_id = obtener_post_id(filename)
-        
-        if post_id:
-            # Guardar la información del comentario en la colección "comentarios" relacionándolo al post
-            comment_data = {
-                "post_id": post_id,
-                "comment": comment,
-                "author": author["_id"],
-                "fecha_creacion": fecha_actual
-            }
-            db.comentarios.insert_one(comment_data)
-            return True
-        else:
-            print("No se encontró el post con el filename especificado.")
-            return False
-    else:
-        print("No se encontró el usuario que publica el comentario.")
-        return False
-
-
-def obtener_post_id(filename):
-    # Obtener el post_id basado en el título del post
-    post = db.posts.find_one({"filename": filename})
-    if post:
-        return post["_id"]
-    else:
-        print("No se encontró el post con el título especificado.")
-        return None
-            
-            
-def obtener_posts():
-    # Utiliza la función aggregate de la variable db.posts para obtener todos los posts y sus colecciones relacionadas
-    posts = db.posts.aggregate([
-        {
-            '$lookup': {
-                'from': 'users',
-                'localField': 'author',
-                'foreignField': '_id',
-                'as': 'author_info'
-            }
-        },
-        {
-            '$addFields': {
-                'likes': 0  # Agregar el campo 'likes' con un contador inicial de 0
-            }
-        }
-    ])
-    
-    # Crear una lista de información de cada post
-    post_info = [(post['_id'], post['filename'], post['title'], post['author_info'][0]['name'], post['fecha_creacion'], post['likes'], post['description']) for post in posts]
-    return post_info
-
-def obtener_comentarios():
-    # Utiliza la función aggregate de la variable db.comentarios para obtener todos los comentarios relacionados a un post_id
-    comments = db.comentarios.aggregate([
-        {
-            '$lookup': {
-                'from': 'users',
-                'localField': 'author',
-                'foreignField': '_id',
-                'as': 'author_info'
-            }
-        },
-        {
-            '$project': {
-                'post_id': 1,  # Agregar el campo 'post_id' a la proyección
-                'comment': 1,
-                'author_name': '$author_info.name',
-                'author_picture': '$author_info.picture',
-                'author_email': '$author_info.email',
-                'fecha_creacion': 1
-            }
-        }
-    ])
-    
-    # Crear una lista de información de cada comentario
-    comment_info = [(comment['_id'], comment['post_id'], comment['comment'], comment['author_name'][0], comment['author_picture'][0], comment['author_email'][0], comment['fecha_creacion']) for comment in comments]
-    return comment_info
 
 # inicio de sesion con google
 GOOGLE_CLIENT_ID = os.getenv('GOOGLE_CLIENT_ID')
@@ -263,14 +130,22 @@ def protected_area():
     # Renderiza los datos y la platilla
     return render_template("dashboard.html", name=name, email=email, picture=picture)
 
-
 @app.route('/logout')
 def logout():
     session.clear()
-    return redirect(url_for('index'))
+    response = make_response(redirect(url_for('index')))
+    response.headers['Cache-Control'] = 'no-cache, no-store, must-revalidate'
+    response.headers['Pragma'] = 'no-cache'
+    response.headers['Expires'] = '0'
+    return response
 # Fin de login con Google ------------------------------
 
-
+@app.route('/delete_post/<post_id>', methods=['POST'])
+def delete_post_route(post_id):
+    return delete_post(post_id) # Llama a la función delete_post de funciones.py
+@app.route('/delete_comment/<comment_id>', methods=['POST'])
+def delete_comment_route(comment_id):
+    return delete_comment(comment_id) # Llama a la función delete_comment de funciones.py
 # Rutas de la aplicación ------------------------------
 @app.route('/')
 def index():
